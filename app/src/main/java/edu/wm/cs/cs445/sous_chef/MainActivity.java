82dd8222598ffd4f13a2f1cc8175f1e1f69c2af7
jlin42 <jlin14@wm.edu>
2024-02-19 19:21:36 -0500

    Initial Setup

---
+++ app/src/main/java/edu/wm/cs/cs445/sous_chef/MainActivity.java
@@ -0,0 +1,14 @@
+package edu.wm.cs.cs445.sous_chef;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
