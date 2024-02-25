34b5be27849e30ca056d8aafd5371fef0ef60ddf
Marshall Wright <fmwright@wm.edu>
2024-02-25 16:00:55 -0500

    Add input box on CreateActivity and start backend for dynamic filter list
    Currently the input box will let the user search their ingredients to find specific ones, and it is supposed to fill the RecyclerView with each ingredient the user wants after pressing the add button.  Right now I still cannot get the Recyclerview to populate at all.

--- app/src/main/java/edu/wm/cs/cs445/sous_chef/CreateActivity.java
+++ app/src/main/java/edu/wm/cs/cs445/sous_chef/CreateActivity.java
@@ -2,8 +2,18 @@ package edu.wm.cs.cs445.sous_chef;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 
 public class CreateActivity extends AppCompatActivity {
@@ -12,5 +22,40 @@ public class CreateActivity extends AppCompatActivity {
         super.onCreate(saveInstanceState);
         setContentView(R.layout.activity_create);
 
+        Button infoBtn = (Button) findViewById(R.id.createRecipeButton);
+        infoBtn.setOnClickListener(v -> {
+            Toast inputInfo = Toast.makeText(CreateActivity.this, "Input ingredients to filter with. Leave blank to search with all of your ingredients", Toast.LENGTH_LONG);
+            inputInfo.show();
+        });
+
+        AutoCompleteTextView ingredientsTextView = (AutoCompleteTextView) findViewById(R.id.ingredients_autocomplete);
+        String ingredients[] = getResources().getStringArray(R.array.user_ingredients);
+        ArrayAdapter<String> ingredientsAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, ingredients);
+        ingredientsTextView.setAdapter(ingredientsAdapter);
+
+        ArrayList<String> filtersList = new ArrayList<>();
+        List<String> ingredientsList = Arrays.asList(ingredients);
+
+        //String[] filtersArr = new String[10];
+        //String[] testArr = {"String", "test", "this sucks"};
+
+        RecyclerView recyclerView = findViewById(R.id.SelectedFilters);
+        IngredientAdapter filterAdapter = new IngredientAdapter(filtersList);
+        recyclerView.setAdapter(filterAdapter);
+
+
+        Button addFilter = (Button) findViewById(R.id.addFilterBtn);
+        addFilter.setOnClickListener(v -> {
+            String newFilter = ingredientsTextView.getText().toString();
+            if (ingredientsList.contains(newFilter)) {
+                filtersList.add(newFilter);
+                filterAdapter.notifyDataSetChanged();
+                ingredientsTextView.setText("");
+            } else {
+                Toast ingredNotFound = Toast.makeText(CreateActivity.this, "Ingredient is not in your list", Toast.LENGTH_SHORT);
+                ingredNotFound.show();
+            }
+
+        });
     }
 }
