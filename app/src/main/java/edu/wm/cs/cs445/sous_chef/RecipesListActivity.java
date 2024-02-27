3b86efa50cd5ab02f2ddcd5ef0518dbcf829c414
Camron Rule <cmrule@wm.edu>
2024-02-27 17:51:36 -0500

    Create RecipesListActivity

---
+++ app/src/main/java/edu/wm/cs/cs445/sous_chef/RecipesListActivity.java
@@ -0,0 +1,17 @@
+package edu.wm.cs.cs445.sous_chef;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RecipesListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipes_list);
+
+        // TODO - figure out how to connect JSON output from API to RecyclerView
+
+    }
+}
