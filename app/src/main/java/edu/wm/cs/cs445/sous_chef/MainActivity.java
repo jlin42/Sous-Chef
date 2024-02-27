a9a6b3fd7ddf17c72313f097a36c88e99afc9a12
jlin42 <jlin14@wm.edu>
2024-02-27 11:07:11 -0500

    Add MainActivity buttons and functionality

--- app/src/main/java/edu/wm/cs/cs445/sous_chef/MainActivity.java
+++ app/src/main/java/edu/wm/cs/cs445/sous_chef/MainActivity.java
@@ -10,37 +10,38 @@ import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button pantry;
+    Button history;
+    Button favorites;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Get a reference to the Protein AutoCompleteTextView in the layout.
-        AutoCompleteTextView proteinTextView = (AutoCompleteTextView) findViewById(R.id.protein_autocomplete);
-        // Get the string array.
-        String[] proteins = getResources().getStringArray(R.array.protein_array);
-        // Create the adapter and set it to the AutoCompleteTextView.
-        ArrayAdapter<String> proteinAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, proteins);
-        proteinTextView.setAdapter(proteinAdapter);
+        pantry = findViewById(R.id.pantry);
+        history = findViewById(R.id.history);
+        favorites = findViewById(R.id.favorites);
 
-        // Get a reference to the Veggie AutoCompleteTextView in the layout.
-        AutoCompleteTextView veggieTextView = (AutoCompleteTextView) findViewById(R.id.veggie_autocomplete);
-        String[] veggies = getResources().getStringArray(R.array.veggie_array);
-        ArrayAdapter<String> veggieAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, veggies);
-        veggieTextView.setAdapter(veggieAdapter);
-
-        // Get a reference to the Starch AutoCompleteTextView in the layout.
-        AutoCompleteTextView starchTextView = (AutoCompleteTextView) findViewById(R.id.starch_autocomplete);
-        String[] starches = getResources().getStringArray(R.array.starch_array);
-        ArrayAdapter<String> starchAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, starches);
-        starchTextView.setAdapter(starchAdapter);
-
-        Button createRecipe = (Button) findViewById(R.id.createRecipeButton);
-        createRecipe.setOnClickListener(new View.OnClickListener() {
+        pantry.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, CreateActivity.class));
+                startActivity(new Intent(MainActivity.this, PantryActivity.class));
+            }
+        });
+
+        history.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, HistoryActivity.class));
+            }
+        });
+
+        favorites.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, FavoriteRecipesActivity.class));
             }
         });
     }
---
+++ app/src/main/java/edu/wm/cs/cs445/sous_chef/PantryActivity.java
@@ -0,0 +1,14 @@
+package edu.wm.cs.cs445.sous_chef;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class PantryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pantry);
+    }
+}
