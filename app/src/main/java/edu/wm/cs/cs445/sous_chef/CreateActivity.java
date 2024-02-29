61449f831deb65f72d94c74477c7e491fea4dbd2
jlin42 <jlin14@wm.edu>
2024-02-29 15:16:33 -0500

    Create InputAdapter for CreateActivity

--- app/src/main/java/edu/wm/cs/cs445/sous_chef/CreateActivity.java
+++ app/src/main/java/edu/wm/cs/cs445/sous_chef/CreateActivity.java
@@ -1,12 +1,14 @@
 package edu.wm.cs.cs445.sous_chef;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
@@ -15,6 +17,8 @@ import java.util.List;
 
 
 public class CreateActivity extends AppCompatActivity {
+    RecyclerView inputBox;
+    InputAdapter inputAdapter;
     @Override
     protected void onCreate(Bundle saveInstanceState) {
         super.onCreate(saveInstanceState);
@@ -29,16 +33,24 @@ public class CreateActivity extends AppCompatActivity {
         ArrayAdapter<String> ingredientsAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, ingredients);
         ingredientsTextView.setAdapter(ingredientsAdapter);
 
+        // Holds inputs
         ArrayList<String> filtersList = new ArrayList<>();
         List<String> ingredientsList = Arrays.asList(ingredients);
+
+        inputBox = findViewById(R.id.ingredientInputs);
+        inputAdapter = new InputAdapter(filtersList);
+        inputBox.setAdapter(inputAdapter);
+        inputBox.setLayoutManager(new LinearLayoutManager(this));
 
 
         Button addFilter = (Button) findViewById(R.id.addFilterBtn);
         addFilter.setOnClickListener(v -> {
             String newFilter = ingredientsTextView.getText().toString();
+            Log.v("test", "test");
             if (ingredientsList.contains(newFilter)) {
                 filtersList.add(String.valueOf(ingredientsTextView.getText()));
                 //TODO: Implement code to add from input to filterview
+                inputAdapter.notifyDataSetChanged();
                 ingredientsTextView.setText("");
             } else {
                 Toast ingredNotFound = Toast.makeText(CreateActivity.this, "Ingredient is not in your list", Toast.LENGTH_SHORT);
