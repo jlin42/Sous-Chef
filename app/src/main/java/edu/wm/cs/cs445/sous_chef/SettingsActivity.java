9e35ce9add07b5c2f0c7c51644f679ad6a189a43
Marshall Wright <fmwright@wm.edu>
2024-02-29 11:09:51 -0500

    Fix navbar size on all screens and Create Settings page
    Settings currently does not have working buttons, just the UI for them.

--- app/src/main/java/edu/wm/cs/cs445/sous_chef/SettingsActivity.java
+++ app/src/main/java/edu/wm/cs/cs445/sous_chef/SettingsActivity.java
@@ -1,8 +1,15 @@
 package edu.wm.cs.cs445.sous_chef;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SettingsActivity extends AppCompatActivity {
 
+    @Override
+    protected void onCreate(Bundle savedInstanceBundle) {
+        super.onCreate(savedInstanceBundle);
+        setContentView(R.layout.activity_settings);
+    }
 
 }
