85305b80622f7b640a2ae4bd7f6c53b5cbae5820
jlin42 <jlin14@wm.edu>
2024-03-05 15:48:40 -0500

    Icon name cleanup

--- app/src/main/java/edu/wm/cs/cs445/sous_chef/BaseFrame.java
+++ app/src/main/java/edu/wm/cs/cs445/sous_chef/BaseFrame.java
@@ -3,6 +3,7 @@ package edu.wm.cs.cs445.sous_chef;
 import static androidx.core.app.ShareCompat.getCallingActivity;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
@@ -11,12 +12,12 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.res.ResourcesCompat;
 import androidx.fragment.app.Fragment;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class BaseFrame extends Fragment {
-
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
