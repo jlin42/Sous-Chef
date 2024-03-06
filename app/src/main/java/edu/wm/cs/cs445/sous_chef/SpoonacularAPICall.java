b3e51ee989983a02ee4e1292395719580ffa687c
Carlo <CarloMehegan@gmail.com>
2024-03-06 16:56:46 -0500

    second api call implemented
    also some refactoring. the second api call is the one to get instructions using recipe id. still need to test these classes within the android environment

--- app/src/main/java/edu/wm/cs/cs445/sous_chef/SpoonacularAPICall.java
+++ app/src/main/java/edu/wm/cs/cs445/sous_chef/SpoonacularAPICall.java
@@ -25,23 +25,93 @@ public class SpoonacularAPICall {
     //want to be able to input a SpoonacularAPIIngredient[] object
         //and get the SpoonacularAPIRecipe[] object back
     private String API_KEY;
-    private String BASE_URL = "https://api.spoonacular.com/recipes/findByIngredients?";
-    private int num_recipes;
-    private String[] ingredients;
 
-    public SpoonacularAPICall(String API_KEY, int num_recipes, String[] ingredients) {
+    public SpoonacularAPICall(String API_KEY) {
         this.API_KEY = API_KEY;
-        this.num_recipes = num_recipes;
-        this.ingredients = ingredients;
     }
 
-    public CompletableFuture<Recipe[]> makeCallAsyncOk() {
-        CompletableFuture<Recipe[]> result = new CompletableFuture<>();
+    public CompletableFuture<SpoonacularAPIRecipe[]> getRecipeByIngredients(int num_recipes, String[] ingredients) {
+        CompletableFuture<SpoonacularAPIRecipe[]> result = new CompletableFuture<>();
         OkHttpClient client = new OkHttpClient();
 
-        // Build the URL with HttpUrl
-        HttpUrl.Builder urlBuilder = HttpUrl.parse(this.buildUrl()).newBuilder();
-        String url = urlBuilder.build().toString();
+        String BASE_URL = "https://api.spoonacular.com/recipes/findByIngredients?";
+        String ingredient_substring = this.buildIngredientSubstring(ingredients);
+
+        //now we just put the url together
+        String url =
+            BASE_URL +
+            "apiKey=" + this.API_KEY +
+            "&number=" + num_recipes +
+            "&ingredients=" + ingredient_substring;
+
+        Request request = new Request.Builder()
+            .url(url)
+            .addHeader("Content-Type", "application/json")
+            .build();
+
+        // Enqueue the asynchronous call
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                // Complete the CompletableFuture exceptionally in case of failure
+                result.completeExceptionally(e);
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                if (!response.isSuccessful()) {
+                    result.completeExceptionally(new IOException("Unexpected code " + response));
+                } else {
+                    // Use Gson to parse the response
+                    Gson gson = new Gson();
+                    SpoonacularAPIRecipe[] recipes = gson.fromJson(response.body().charStream(), SpoonacularAPIRecipe[].class);
+                    result.complete(recipes); // Complete the CompletableFuture with the result
+                }
+            }
+        });
+
+        return result;
+    }
+
+    //helper function for the recipe name api call
+    private String buildIngredientSubstring(String[] ingredients) {
+		/*
+		awesome string concatenation
+		example: "https://api.spoonacular.com/recipes/
+			findByIngredients?number=1&ingredients=apples,+flour,+sugar&apiKey=123"
+		*/
+
+		/*
+		first we put the ingredient substring together
+		example: "apples,+flour,+sugar"
+		this is a bit crude but should be fine for our purposes
+		as long as theres no special characters and there arent a million ingredients
+		*/
+        String ingredient_substring = "";
+        for (String ingredient : ingredients) {
+            ingredient_substring += ingredient + ",+";
+
+        }
+
+        if (ingredient_substring.length() > 0) {
+            //remove the extra ",+" at the end
+            ingredient_substring = ingredient_substring.substring(0, ingredient_substring.length() - 2);
+        }
+
+        return ingredient_substring;
+    }
+
+    //instructions api call
+    public CompletableFuture<String> getInstructionsById(int id) {
+        CompletableFuture<String> result = new CompletableFuture<>();
+        OkHttpClient client = new OkHttpClient();
+
+        //this url is a lot easier to build so lets one line it
+        String url = String.format(
+            "https://api.spoonacular.com/recipes/%d/analyzedInstructions?apiKey=%s",
+            id,
+            this.API_KEY
+        );
 
         Request request = new Request.Builder()
                 .url(url)
@@ -61,10 +131,8 @@ public class SpoonacularAPICall {
                 if (!response.isSuccessful()) {
                     result.completeExceptionally(new IOException("Unexpected code " + response));
                 } else {
-                    // Use Gson to parse the response
-                    Gson gson = new Gson();
-                    Recipe[] recipes = gson.fromJson(response.body().charStream(), Recipe[].class);
-                    result.complete(recipes); // Complete the CompletableFuture with the result
+                    String parsedInstructions = formatInstructions(response.body().string());
+                    result.complete(parsedInstructions);
                 }
             }
         });
@@ -72,36 +140,61 @@ public class SpoonacularAPICall {
         return result;
     }
 
-    private String buildUrl() {
-		/*
-		awesome string concatenation
-		example: "https://api.spoonacular.com/recipes/
-			findByIngredients?number=1&ingredients=apples,+flour,+sugar&apiKey=123"
-		*/
+    //helper function for the instructions api call
+    private String formatInstructions(String instructions) {
+        Gson gson = new Gson();
+        SpoonacularAPIInstructionList[] instructionList = gson.fromJson(instructions, SpoonacularAPIInstructionList[].class);
+        StringBuilder formattedInstructions = new StringBuilder();
 
-		/*
-		first we put the ingredient substring together
-		example: "apples,+flour,+sugar"
-		this is a bit crude but should be fine for our purposes
-		as long as theres no special characters and there arent a million ingredients
-		*/
-        String ingredient_substring = "";
-        for (String ingredient : this.ingredients) {
-            ingredient_substring += ingredient + ",+";
-
-        }
-        if (ingredient_substring.length() > 0) {
-            //remove the extra ",+" at the end
-            ingredient_substring = ingredient_substring.substring(0, ingredient_substring.length() - 2);
+        for (SpoonacularAPIInstruction instruction : instructionList[0].getInstructions()) {
+            // Assuming 'number' is an appropriately formatted string or can be directly used here.
+            // Append the instruction number and step to the StringBuilder.
+            // Then append a newline character to separate each instruction.
+            formattedInstructions
+                    .append(instruction.getNumber())
+                    .append(". ")
+                    .append(instruction.getStep())
+                    .append("\n");
         }
 
-        //now we just put the url together
-        String url =
-            this.BASE_URL +
-            "apiKey=" + this.API_KEY +
-            "&number=" + this.num_recipes +
-            "&ingredients=" + ingredient_substring;
+        // Convert the StringBuilder content to a String and return it
+        return formattedInstructions.toString();
+    }
+}
 
-        return url;
+
+//the InstructionList and Instruction subclasses are used for Gson parsing
+//lowkey ignore them
+class SpoonacularAPIInstructionList {
+    public SpoonacularAPIInstruction[] steps;
+
+    public SpoonacularAPIInstruction[] getInstructions() {
+        return steps;
+    }
+
+    public void setInstructions(SpoonacularAPIInstruction[] steps) {
+        this.steps = steps;
+    }
+}
+
+//and another subclass for Gson
+class SpoonacularAPIInstruction {
+    public String number;
+    public String step;
+
+    public String getNumber() {
+        return number;
+    }
+
+    public String getStep() {
+        return step;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public void setStep(String step) {
+        this.step = step;
     }
 }
